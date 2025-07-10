4581d85d03e37ebaad296ae1f8493894075495ca
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-10 17:44:05 +0530

    <core> adding runtime checks to player::bet and player::credit methods

--- src/player.cpp
+++ src/player.cpp
@@ -17,14 +17,21 @@ void Player::addCard(const Cards::Card &card) {
   return {*this->hand[0], *this->hand[1]};
 }
 
-void Player::bet(int bet_amount) {
+void Player::resetHand() noexcept { this->hand.fill(std::nullopt); }
+
+void Player::bet(int bet_amount) noexcept(false) {
   if (bet_amount > chips) {
     throw std::invalid_argument("Bet amount cannot be greater than balance.");
+  } else if (bet_amount <= 0) {
+    throw std::invalid_argument("Bet amount must be positive.");
   }
   this->chips -= bet_amount;
 }
 
-void Player::credit(int credit_amount) noexcept {
+void Player::credit(int credit_amount) noexcept(false) {
+  if (credit_amount < 0) {
+    throw std::invalid_argument("Credit amount must be positive.");
+  }
   this->chips += credit_amount;
 }
 
