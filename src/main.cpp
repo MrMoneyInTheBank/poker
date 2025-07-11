87b0d9ba5f1561f39adac4012ae855e1954c8c75
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-11 18:11:19 +0530

    <core> PoC for initial game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include "deck.hpp"
+#include "player.hpp"
+#include "poker.hpp"
+#include <array>
 #include <iostream>
 
 constexpr std::array<std::string_view, 4> SuitNames{"♣", "♦", "♥", "♠"};
@@ -16,3 +20,22 @@ std::ostream &operator<<(std::ostream &os, const Cards::Card &c) {
   return os << to_string(c.rank) << to_string(c.suit);
 }
 
+int main() {
+  std::cout << "Welcome to poker!\n";
+  Cards::Deck deck;
+  Player human(50);
+  Player computer(50);
+
+  Poker::Table<2> poker_table(deck);
+  poker_table.add_player(human);
+  poker_table.add_player(computer);
+
+  poker_table.deal_cards();
+
+  std::array<Cards::Card, 2> ansh = poker_table.players[0]->showHand();
+  std::array<Cards::Card, 2> comp = poker_table.players[1]->showHand();
+  std::cout << "Ansh hand\n";
+  std::cout << ansh[0] << ", " << ansh[1];
+  std::cout << "\nComp hand\n";
+  std::cout << comp[0] << ", " << comp[1] << "\n";
+}
