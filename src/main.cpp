260cf3042a27c8f8995c15ad3001b7e31c912182
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-11 18:10:05 +0530

    <core> repr function for card enums

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,10 @@ constexpr std::array<std::string_view, 4> SuitNames{"♣", "♦", "♥", "♠"};
 constexpr std::array<std::string_view, 13> RankNames{
     "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
 
+constexpr std::string_view to_string(Cards::Suit s) {
+  return SuitNames[static_cast<std::size_t>(s)];
+}
+constexpr std::string_view to_string(Cards::Rank r) {
+  return RankNames[static_cast<std::size_t>(r) - 2];
+}
+
