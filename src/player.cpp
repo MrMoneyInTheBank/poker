96bb0f60803cf4f086a6ee3958bfc4128e753b20
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-11 18:06:32 +0530

    <style> change function implementations to snake_case

--- src/player.cpp
+++ src/player.cpp
@@ -1,13 +1,13 @@
 #include "player.hpp"
 
-[[nodiscard]] Player::PocketCards Player::showHand() const noexcept(false) {
+[[nodiscard]] Player::PocketCards Player::show_hand() const noexcept(false) {
   if (not this->hand[0].has_value() || not this->hand[1].has_value()) {
     throw std::logic_error("Can't show empty/partial hand");
   }
   return {this->hand[0].value(), this->hand[1].value()};
 }
 
-void Player::resetHand() noexcept { this->hand.fill(std::nullopt); }
+void Player::reset_hand() noexcept { this->hand.fill(std::nullopt); }
 
 void Player::bet(int bet_amount) noexcept(false) {
   if (bet_amount > chips) {
@@ -25,6 +25,6 @@ void Player::credit(int credit_amount) noexcept(false) {
   this->chips += credit_amount;
 }
 
-[[nodiscard]] int Player::checkBalance() const noexcept { return this->chips; }
+[[nodiscard]] int Player::check_balance() const noexcept { return this->chips; }
 
-[[nodiscard]] bool Player::isBroke() const noexcept { return this->chips == 0; }
+[[nodiscard]] bool Player::is_broke() const noexcept { return this->chips == 0; }
