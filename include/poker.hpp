9d1f0a3818f7298d3b146cac5e6b32f89be6a5a7
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-26 18:39:38 +0530

    <core> remove table size adjustment code

--- include/poker.hpp
+++ include/poker.hpp
@@ -3,47 +3,27 @@
 #include "deck.hpp"
 #include "player.hpp"
 #include <array>
-#include <stdexcept>
 
 namespace Poker {
 
 template <std::size_t seats>
   requires(seats >= 2 && seats < 10)
 class Table {
-  using Players = std::array<std::optional<Player>, seats>;
+  using Players = std::array<Player, seats>;
 
 public:
   Table() = default;
-  Table(const Cards::Deck &deck) : deck(deck){};
-
-  template <typename P>
-    requires std::same_as<std::remove_cvref_t<P>, Player>
-  void add_player(P &&player) noexcept(false) {
-    typename Players::iterator slot =
-        std::find(this->players.begin(), this->players.end(), std::nullopt);
-    if (slot == this->players.end()) {
-      throw std::runtime_error("Cannot add player to full table.");
-    }
-
-    *slot = std::forward<P>(player);
-  }
-
-  bool table_full() const noexcept {
-    typename Players::const_iterator slot =
-        std::find(this->players.begin(), this->players.end(), std::nullopt);
-
-    return slot == this->players.end();
-  }
-
-  // TODO
-  // probably best to make this a private method
-  // later and invoke it via a main game loop
+  Table(const Cards::Deck &deck, const std::array<Player, seats> &players)
+      : players(players), deck(deck){};
   void deal_cards() noexcept(false) {
-    for (std::size_t i = 0; i < 2 * this->players.size(); i++) {
-      if (this->players[i % seats].has_value()) {
-        this->players[i % seats].value().add_card(this->deck.draw());
-      }
+    for (std::size_t i = 0; i < 2 * seats; i++) {
+      this->players[i % seats].add_card(this->deck.draw());
     }
+  }
+
+
+  }
+
   }
 
   Players players;
