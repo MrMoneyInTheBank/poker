98d728662d43fa79ac5fb4d377e0847774bd90ef
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-26 17:57:11 +0530

    <refactor> style changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 
 constexpr std::array<std::string_view, 4> SuitNames{"♣", "♦", "♥", "♠"};
-
 constexpr std::array<std::string_view, 13> RankNames{
     "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
 
