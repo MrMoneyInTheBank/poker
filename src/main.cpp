3be599ea73c2c1e20cdd93e5859fa0e3516c9325
MrMoneyInTheBank <singh.ansh@husky.neu.edu>
2025-07-27 23:06:17 +0530

    <core> game loop text

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,31 @@
 int main() {
   std::cout << "Welcome to poker!\n";
   Cards::Deck deck;
+
   Player human(50);
   Player computer(50);
 
   Poker::Table<2> poker_table(deck, {human, computer});
 
-  poker_table.deal_cards();
+  poker_table.deal_pocket_cards();
 
   std::array<Cards::Card, 2> ansh = poker_table.players[0].show_hand();
   std::array<Cards::Card, 2> comp = poker_table.players[1].show_hand();
-  std::cout << "Ansh hand\n";
-  std::cout << ansh[0] << ", " << ansh[1];
-  std::cout << "\nComp hand\n";
-  std::cout << comp[0] << ", " << comp[1] << "\n";
 
+  std::cout << "Ansh hand\n";
+  std::cout << ansh[0] << " " << ansh[1];
+  std::cout << "\nComp hand\n";
+  std::cout << comp[0] << " " << comp[1] << "\n";
+
+  poker_table.flop();
+  poker_table.turn();
+  poker_table.river();
+
+  std::cout << "\nCommunity cards\n";
+  for (auto card : poker_table.community_cards) {
+    if (card.has_value()) {
+      std::cout << card.value() << " ";
+    }
+  }
+  std::cout << std::endl;
 }
